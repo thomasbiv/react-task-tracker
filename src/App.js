70b10908c8c6db839759183a628e6da15ff729fc
Thomas Bivins <bivinsthomas@gmail.com>
2021-12-27 14:41:39 -0500

    Add task functionality and form toggling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState } from 'react'
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
       {
@@ -25,6 +26,13 @@ function App() {
       },
     ])
 
+  //Add Task Function
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+  
   //Delete Task Function
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id)) //display all tasks with an id that is not the passed id
@@ -36,8 +44,8 @@ function App() {
 
   return (
     <div className='container'>
-      <Header title = 'Task Master'/>
-      <AddTask />
+      <Header title = 'Task Master' onAdd = {() => setShowAddTask(!showAddTask)} showAdd = {showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {
         tasks.length === 0
         ? 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,10 @@ import React from 'react'
 import Button from './Button'
 
 const Header = (props) => {
-    const addAction = () => {
-        console.log("*add button functionality*")
-    }
-    
     return (
         <header className='header'>
             <h1>{props.title}</h1>
-            <Button title = 'Add' onClick = {addAction}/>
+            <Button title = {props.showAdd ? "Close" : "Add"} onClick = {props.onAdd}/>
         </header>
     )
 }
